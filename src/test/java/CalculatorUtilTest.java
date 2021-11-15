c834e10edfc9a414dd54d3b2a21eedebff4c3609
Thanisha M <thanisha@Thanishas-MacBook-Pro.local>
2021-11-15 14:27:12 +0530

    Modification in CaculatorUtilTest.java class

--- src/test/java/CalculatorUtilTest.java
+++ src/test/java/CalculatorUtilTest.java
@@ -2,29 +2,29 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CalculatorUtilTest {
-    CalculatorPage c=new CalculatorPage();
+    CalculatorPage object =new CalculatorPage();
     @Test
     public void shouldAddTwoNumbers()
     {
-        int sum = c.getSum(10,20);
+        int sum = object.getSum(10,20);
         Assert.assertEquals(sum,30);
     }
     @Test
     public void shouldMultiplyTwoNumbers()
     {
-        int result = c.getResultOfMultiplication(10,20);
+        int result = object.getResultOfMultiplication(10,20);
         Assert.assertEquals(200,result);
     }
     @Test
     public void shouldDivideTwoNumbers()
     {
-        int result = c.getResultOfDivision(300,10);
+        int result = object.getResultOfDivision(300,10);
         Assert.assertEquals(30,result);
     }
     @Test
     public void shouldSubtractTwoNumbers()
     {
-        int result = c.getDifference(20,10);
+        int result = object.getDifference(20,10);
         Assert.assertEquals(10,result);
     }
 
